e560341fce54fcd3336f5ea653eaec9824f2ad23
kaspervanderhulst <kasper.vanderhulst@student.ucll.be>
2020-10-18 14:49:57 +0000

    some fixes to both backup and sshguard

--- scripts/incron_config.sh
+++ scripts/incron_config.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find /etc -type d ! -regex '.*/.git.*' ! -regex '.*/mrt.*' -print0 | xargs -0 -I{} echo "{} IN_CLOSE_WRITE /etc/scripts/backup.sh \$@/\$#" > /etc/incron.d/etc.conf
+find /etc -type d ! -regex '.*/.git.*' ! -regex '.*/mrt.*' -print0 | xargs -0 -I{} echo "{} IN_CLOSE_WRITE,recursive=false /etc/scripts/backup.sh \$@/\$#" > /etc/incron.d/etc.conf
